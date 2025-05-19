76c295ef3549e026f24a0baf5158f93a85904ee3
RamInstitute-Exam <raminstituteexams@gmail.com>
2025-05-19 12:34:36 -0500

    config uodate

--- src/config/API.jsx
+++ src/config/API.jsx
@@ -2,7 +2,8 @@ import axios from "axios"
 
 
 const API = axios.create({
-baseURL:"http://localhost:5000/",
+// baseURL:"http://localhost:5000/",
+baseURL:"https://ram-institute-backend.onrender.com/",
 // baseURL: "https://website-backend-production-e152.up.railway.app/",
    withCredentials: true,
 })
